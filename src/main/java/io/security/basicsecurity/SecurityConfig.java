26a6bdb3ee4ac6801d294840a120e4d15d13098b
SongMinJun <dkaskgkdua@gmail.com>
2021-01-12 00:05:37 +0900

    - 세션 고정 보호 - 동시세션 제어

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -30,9 +30,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // 인가정책
         http
                 .authorizeRequests()            // 요청에 대한 보안검사
-                .anyRequest().authenticated();  // 모든 요청에도 인증을 받도록함.
+                .anyRequest().authenticated()  // 모든 요청에도 인증을 받도록함.
         // 인증정책
-        http
+        .and()
                 .formLogin()                   // form 로그인 방식
                 //.loginPage("/loginPage")
                 .defaultSuccessUrl("/")
@@ -55,8 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     }
                 })
                 .permitAll()
-        ;
-        http
+        .and()
                 .logout()           // 기본적(원칙)으로 post방식임
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("login") // logoutSuccessHandler로 더욱 커스텀 가능
@@ -74,11 +73,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     }
                 })
                 .deleteCookies("remember-me")  //  로그아웃할때 쿠키 삭제됨
+                /**
+                 *  JSESSIONID를 삭제해도
+                 *  remember-me 쿠키 값이 있으면
+                 *  시큐리티에서 체크하고 인증처리를 함.
+                 *  -> 로그인이 된다는 말임
+                 */
         .and()
                 .rememberMe()
                 .rememberMeParameter("remember-me")         // default는 remember-me -> 커스텀 가능(체크박스와 이름 동일하게)
                 .tokenValiditySeconds(5000)                 // default는 14일, 현재 5000초 설정해놨음
-                .alwaysRemember(true)                       // default false, 기능이 활성화되지 않아도 항상 실행여부
-                .userDetailsService(userDetailsService);    // 확인해주는 클래스 넣어줌
+                .alwaysRemember(false)                       // default false, 기능이 활성화되지 않아도 항상 실행여부
+                .userDetailsService(userDetailsService)    // 확인해주는 클래스 넣어줌
+        .and()
+                .sessionManagement()
+                .sessionFixation()          // 세션 고정 보호
+                .changeSessionId()          // 세션 고정 보호 정책
+                .maximumSessions(1)         // 최대 세션 동시접속 수
+                .maxSessionsPreventsLogin(false);  // 동시 로그인 차단함, false : 기존 세션만료(default)
     }
 }
