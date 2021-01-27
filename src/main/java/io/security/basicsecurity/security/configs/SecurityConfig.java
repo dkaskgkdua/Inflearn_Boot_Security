3966962bbae61fef8f05ef02f2aa8d525864c4d2
SongMinJun <dkaskgkdua@gmail.com>
2021-01-27 20:31:40 +0900

    webignore 셋팅

--- src/main/java/io/security/basicsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/security/configs/SecurityConfig.java
@@ -1,9 +1,11 @@
 package io.security.basicsecurity.security.configs;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -11,6 +13,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    // webIgnore 설정 - 정적 리소스 관리 : 보안필터도 안거침(비용적인 측면에서 절약)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
     // 메모리에 임의 유저정보 - 테스트용
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
