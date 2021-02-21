edd83e3916fa7db4b296e0c55ea8b5bc38997dde
han-jungwon <rhehf1024@naver.com>
2021-02-22 02:25:00 +0900

    메소드 기반 인가 처리 구현 - DB연동
    - data loader 구현 필요

--- src/main/java/io/security/basicsecurity/aopsecurity/AopSecurityController.java
+++ src/main/java/io/security/basicsecurity/aopsecurity/AopSecurityController.java
@@ -1,6 +1,7 @@
 package io.security.basicsecurity.aopsecurity;
 
 import io.security.basicsecurity.domain.dto.AccountDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,8 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.security.Principal;
 
 @Controller
+@RequiredArgsConstructor
 public class AopSecurityController {
-
+    private final AopMethodService aopMethodService;
+    
     @GetMapping("/preAuthorize")
     @PreAuthorize("hasRole('ROLE_USER') and #accountDto.username == principal.username")
     public String preAuthorize(AccountDto accountDto, Model model, Principal principal) {
@@ -18,4 +21,22 @@ public class AopSecurityController {
 
         return "aop/method";
     }
+    
+    @GetMapping("/methodSecured")
+    public String methodSecured(Model model) {
+        aopMethodService.methodSecured();
+        model.addAttribute("method", "Success MethodSecured");
+
+        return "aop/method";
+    }
+    @GetMapping("/methodSecuredManager")
+    public String methodSecuredManager(Model model) {
+        aopMethodService.methodSecuredManager();
+        model.addAttribute("method", "Success methodSecuredManager");
+
+        return "aop/method";
+    }
+
+
+    
 }
--- src/main/java/io/security/basicsecurity/security/configs/MethodSecurityConfig.java
+++ src/main/java/io/security/basicsecurity/security/configs/MethodSecurityConfig.java
@@ -1,16 +1,38 @@
 package io.security.basicsecurity.security.configs;
 
+import io.security.basicsecurity.security.factory.MethodResourcesFactoryBean;
+import io.security.basicsecurity.service.SecurityResourceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.access.method.MapBasedMethodSecurityMetadataSource;
 import org.springframework.security.access.method.MethodSecurityMetadataSource;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
+import java.util.List;
+import java.util.Map;
+
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@RequiredArgsConstructor
 public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration{
+    private final SecurityResourceService securityResourceService;
+
     @Override
     protected MethodSecurityMetadataSource customMethodSecurityMetadataSource() {
-        return new MapBasedMethodSecurityMetadataSource();
+        return mapBasedMethodSecurityMetadataSource();
+    }
+    @Bean
+    public MapBasedMethodSecurityMetadataSource mapBasedMethodSecurityMetadataSource() {
+        return new MapBasedMethodSecurityMetadataSource(methodResourcesFactoryBean().getObject());
+    }
+    @Bean
+    public MethodResourcesFactoryBean methodResourcesFactoryBean() {
+        MethodResourcesFactoryBean methodResourcesFactoryBean = new MethodResourcesFactoryBean();
+        methodResourcesFactoryBean.setSecurityResourceService(securityResourceService);
+        return methodResourcesFactoryBean;
+
     }
 }
